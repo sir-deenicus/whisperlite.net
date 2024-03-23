9f7ce4889549b948211b1b66bf46e87a6682b341
Deen Abiola <sir.deenicus@gmail.com>
2024-03-23 21:10:15 +0100

    Add project files.

---
+++ NativeMethods.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WhisperLite.Net
+{
+    
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void WhisperNewSegmentCallback(IntPtr ctx, IntPtr state, int n_new, IntPtr user_data);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void WhisperProgressCallback(IntPtr ctx, IntPtr state, int progress, IntPtr user_data);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate byte WhisperEncoderBeginCallback(IntPtr ctx, IntPtr state, IntPtr user_data);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void WhisperLogitsFilterCallback(IntPtr ctx, IntPtr state, IntPtr tokens, int n_tokens, IntPtr logits, IntPtr user_data);
+ 
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate byte GgmlAbortCallback(IntPtr data);
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WhisperContextParams
+    {
+        public byte UseGPU;
+        public int GPUDevice;
+    }
+
+    public enum WhisperSamplingStrategy
+    {
+        WhisperSamplingGreedy,      // similar to OpenAI's GreedyDecoder
+        WhisperSamplingBeamSearch, // similar to OpenAI's BeamSearchDecoder
+    } 
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Greedy
+    {
+        public int best_of;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct BeamSearch
+    {
+        public int beam_size;
+        public float patience;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WhisperFullParams
+    {
+        public WhisperSamplingStrategy strategy;
+        public int n_threads;
+        public int n_max_text_ctx;
+        public int offset_ms;
+        public int duration_ms;
+        public byte translate;
+        public byte no_context;
+        public byte no_timestamps;
+        public byte single_segment;
+        public byte print_special;
+        public byte print_progress;
+        public byte print_realtime;
+        public byte print_timestamps;
+        public byte token_timestamps;
+        public float thold_pt;
+        public float thold_ptsum;
+        public int max_len;
+        public byte split_on_word;
+        public int max_tokens;
+        public byte speed_up;
+        public byte debug_mode;
+        public int audio_ctx;
+        public byte tdrz_enable;
+ 
+        public IntPtr initial_prompt;
+        public IntPtr prompt_tokens;
+        public int prompt_n_tokens;
+ 
+        public IntPtr language;
+        public byte detect_language;
+        public byte suppress_blank;
+        public byte suppress_non_speech_tokens;
+        public float temperature;
+        public float max_initial_ts;
+        public float length_penalty;
+        public float temperature_inc;
+        public float entropy_thold;
+        public float logprob_thold;
+        public float no_speech_thold;
+        public Greedy greedy;
+        public BeamSearch beam_search;
+        public WhisperNewSegmentCallback new_segment_callback;
+
+        public IntPtr new_segment_callback_user_data;
+        public WhisperProgressCallback progress_callback;
+        public IntPtr progress_callback_user_data;
+        public WhisperEncoderBeginCallback encoder_begin_callback;
+        public IntPtr encoder_begin_callback_user_data;
+        public GgmlAbortCallback abort_callback;
+        public IntPtr abort_callback_user_data;
+        public WhisperLogitsFilterCallback logits_filter_callback;
+        public IntPtr logits_filter_callback_user_data;
+        public IntPtr grammar_rules;
+        public UIntPtr n_grammar_rules;
+        public UIntPtr i_start_rule;
+        public float grammar_penalty;
+    }
+
+    public static class NativeMethods
+    {
+        const string libraryName = "whisper.dll"; 
+
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern WhisperContextParams whisper_context_default_params(); 
+        
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        public static extern IntPtr whisper_init_from_file_with_params(string path, WhisperContextParams parameters);
+ 
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern WhisperFullParams whisper_full_default_params(WhisperSamplingStrategy strategy);
+
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int whisper_full(IntPtr ctx, WhisperFullParams parameters, float[] samples, int n_samples);
+ 
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int whisper_full_n_segments(IntPtr ctx);
+          
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr whisper_full_get_segment_text(IntPtr ctx, int i_segment);
+
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void whisper_free(IntPtr ctx);
+ 
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int whisper_full_n_tokens(IntPtr ctx, int i_segment);
+ 
+        [DllImport(libraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int whisper_full_get_token_id(IntPtr ctx, int i_segment, int i_token);        
+    }
+}
---
+++ Whisper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+namespace WhisperLite.Net
+{
+    public class Whisper : IDisposable
+    {
+        public IntPtr WhisperContext { get; private set; }
+        public WhisperFullParams parameters;
+
+        public List<int> promptTokens = new List<int>();
+
+        public float[] prevAudioData;
+
+        int sampleRate = 16000;
+
+        int keepSamplesLen = 0;
+
+        int previousSamplesLen = 0;
+
+        public string Language
+        {
+            get
+            {
+                if (parameters.language == IntPtr.Zero)
+                    return "";
+                return Marshal.PtrToStringAnsi(parameters.language);
+            }
+        }
+         
+        public Whisper(string path, bool useGPU = false, float keepAudioSecs = 0.0f)
+        {
+            WhisperContextParams cparameters = NativeMethods.whisper_context_default_params();
+            cparameters.UseGPU = useGPU ? (byte)1 : (byte)0; 
+
+            WhisperContext = NativeMethods.whisper_init_from_file_with_params(path, cparameters);
+            if (WhisperContext == IntPtr.Zero)
+            {
+                throw new Exception("Failed to initialize Whisper from file");
+            }
+  
+            keepSamplesLen = (int)(keepAudioSecs * sampleRate); 
+            prevAudioData = new float[keepSamplesLen];
+        }
+
+        public void SetKeepAudioSecs(float keepAudioSecs)
+        { 
+            keepSamplesLen = (int)(keepAudioSecs * sampleRate);
+            prevAudioData = new float[keepSamplesLen];
+        }
+
+        public void InitToDefaultParameters(WhisperSamplingStrategy strategy = WhisperSamplingStrategy.WhisperSamplingGreedy, bool DoTranslate = false, int NumThreads = 4, string Language = "en")
+        {
+            parameters = NativeMethods.whisper_full_default_params(strategy);  
+            parameters.translate = DoTranslate ? (byte)1 : (byte)0;
+            parameters.n_threads = NumThreads;
+            parameters.language = Marshal.StringToHGlobalAnsi(Language);
+        }
+
+        public string TranscribeAudio(float[] samples, bool keepAudio = false)
+        {  
+            float[] audiosamples = (previousSamplesLen != 0) ? prevAudioData.Take(previousSamplesLen).Concat(samples).ToArray() : samples;
+
+            IntPtr promptptr = IntPtr.Zero;
+
+            if(previousSamplesLen != 0)
+            { 
+                promptptr = Marshal.AllocHGlobal(promptTokens.Count * sizeof(int));
+                Marshal.Copy(promptTokens.ToArray(), 0, promptptr, promptTokens.Count);
+
+                parameters.prompt_n_tokens = promptTokens.Count;
+                parameters.prompt_tokens = promptptr;  
+            }
+
+            NativeMethods.whisper_full(WhisperContext, parameters, audiosamples, audiosamples.Length);
+ 
+            if(promptptr != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(promptptr); 
+                promptptr = IntPtr.Zero;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            promptTokens.Clear();
+
+            if(keepAudio)
+            {
+                var keeplen = Math.Min(keepSamplesLen, samples.Length);
+                previousSamplesLen = keeplen; 
+                Array.Copy(samples, samples.Length - keeplen, prevAudioData, 0, keeplen); 
+            }
+
+            var nsegments = NativeMethods.whisper_full_n_segments(WhisperContext);
+            for(int i = 0; i < nsegments; i++)
+            {
+                var segment = NativeMethods.whisper_full_get_segment_text(WhisperContext, i);  
+                sb.Append(Marshal.PtrToStringAnsi(segment)); 
+
+                if(keepAudio)
+                {
+                    var tokenslen = NativeMethods.whisper_full_n_tokens(WhisperContext, i); 
+                    for(int j = 0; j < tokenslen; j++)
+                    {
+                        promptTokens.Add(NativeMethods.whisper_full_get_token_id(WhisperContext, i, j));
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        public void Dispose()
+        {
+            if (parameters.language != IntPtr.Zero)
+            {
+                Marshal.FreeHGlobal(parameters.language);
+                parameters.language = IntPtr.Zero;
+            }
+
+            if (WhisperContext != IntPtr.Zero)
+            {
+                NativeMethods.whisper_free(WhisperContext);
+                WhisperContext = IntPtr.Zero;
+            }  
+        }
+    }
+}
+          
